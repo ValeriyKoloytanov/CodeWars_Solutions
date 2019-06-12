5bb405de70bdd1e342236b0de22adc8ba1f4faf6
Valera K <47101681+ValeriyKoloytanov@users.noreply.github.com>
2019-06-12 13:30:38 +0300

    Keep Hydrated

---
+++ Keep Hydrated.java
@@ -0,0 +1,10 @@
+public class KeepHydrated  {
+
+    public int Liters(double time)  {
+        float litr=(float)(time*0.5);
+        int result=(int)(Math.floor(litr));
+        return result;
+
+    }
+
+}
